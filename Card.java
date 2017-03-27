14fe5b0b48edfeb4ec6313b29b46a93978492791
Lykke Lewin <lykkelewin@gmail.com>
2017-03-27 14:15:19 +0200

    Nya kommentarer

--- Card.java
+++ Card.java
@@ -1,6 +1,11 @@
 package teachMePoker;
 
 import javax.swing.Icon;
+/**
+ * Representation of a playing card
+ * @author Lykke Levin
+ *
+ */
 
 public class Card {
 	
@@ -8,25 +13,37 @@ public class Card {
 	private int cardValue;
 	private Icon cardIcon;
 
+	/**
+	 * Creates a Card with a suit, value and picture 
+	 * @param suit "Hearts", "Diamonds", "Clubs", "Spades"
+	 * @param value 2,3,4,5,6,7,8,9,10,11,12,13,14
+	 * @param cardIcon 
+	 */
 	public Card(String suit, int value, Icon cardIcon){
 		this.cardSuit = suit;
 		this.cardValue = value;
 		this.cardIcon = cardIcon;
 	}
-	
+	/**
+	 * Returns the value of the card. 
+	 * @return cardValue
+	 */
 	public int getCardValue(){
 		return cardValue;
 	}
-	
+	/**
+	 * Returns the suit of the card.
+	 * @return cardSuit
+	 */
 	public String getCardSuit(){
 		return cardSuit;
 	}
-	
+	/**
+	 * Returns the icon of the card.
+	 * @return cardIcon
+	 */
 	public Icon getCardIcon(){
 		return cardIcon;
 	}
 	
-	public static void main(String [] args){
-		
-	}
 }
