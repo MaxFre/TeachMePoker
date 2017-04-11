a242aae6af26cc64221e628ba7e411567278d8b0
Vecochan <vedrana.zeba@gmail.com>
2017-04-11 12:08:21 +0200

    kommenterad
    main borttagen, remove borttagen

--- src/deck/Deck.java
+++ src/deck/Deck.java
@@ -5,53 +5,64 @@ import java.util.ArrayList;
 import java.util.Collections;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+/**
+ * A deck of cards
+ * @author Vedrana Zeba
+ */
 
 public class Deck {
 	private ArrayList<Card> deck = new ArrayList<Card>();
 	private ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 
+
+	/**
+	 * Creates a deck of cards
+	 */
 	public Deck() {
 		createDeck();
 	}
 
+
+	/**
+	 * Shuffles the deck
+	 */
 	public void shuffle() {
 		Collections.shuffle(deck);
 	}
-
+ 
+	
+	/**
+	 * Returns the reference of the card that's being removed
+	 */
 	public Card getCard() {
 		return deck.remove(0);
-		//return deck.get(0);
 	}
 
-	public Card removeCard() {
-		return deck.remove(0);
-	}
 
-	public int getNumberOfCardsInDeck(){
+	/**
+	 * Returns the current size of the deck
+	 */
+	public int getNumberOfCardsInDeck() {
 		return deck.size();
 	}
 
-	public void createDeck(){
+
+	/**
+	 * Creates a deck
+	 */
+	public void createDeck() {
 		deck = new ArrayList<Card>();
 
-		for (Suit suit: Suit.values()) {
+		for(Suit suit: Suit.values()) {
 			try {
-				for (CardValue card: CardValue.values()) {
+				for(CardValue card: CardValue.values()) {
 					deck.add(new Card(suit, card,
 							new ImageIcon(ImageIO.read(classLoader.getResourceAsStream("images/" + card.getCardValue() + suit.getSuitLetter() + ".png")))));
 				}
 
-			} catch (IOException e) {
+			} catch(IOException e) {
 				e.printStackTrace();
 			}
-		}
-	}
-	
-	public static void main(String [] args){
-		Deck deck = new Deck ();
-		for(int i = 0 ; i < 52; i++){
-			System.out.println(deck.getCard().getCardValue() + " of " + deck.getCard().getCardSuit());
-			deck.removeCard();
 		}
 	}
 }
