250ac8979c57109a00b843d70f84b938e4b0ddd0
MaxFre <mafr@live.se>
2017-04-12 21:06:14 +0200

    patches
    asd

--- src/hand/hand.java
+++ src/hand/hand.java
@@ -1,81 +1,60 @@
 package hand;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import deck.Card;
 import deck.Deck;
-
-public class hand {
-	private Calc calc;
-	private Card card;
-	private Deck deck;
-	private ArrayList<Card> Cards = new ArrayList<Card>();
-	private ArrayList<String> aiCards = new ArrayList<String>();  
-	private ArrayList<String> toHighlight = new ArrayList<String>();  
+/**
+ * The hand-class that will guide and help the noob player.
+ * @author Max Frennessen
+ * 17-04-12
+ * @version 1.5
+ */
+public class Hand {
+	private HandCalculation calc;
+	private ArrayList<Card> cards = new ArrayList<Card>();
+	private ArrayList<String> aiCards = new ArrayList<String>();
+	private ArrayList<String> toHighlight = new ArrayList<String>();
 	
-	public hand() {
-	
-		
-	for(int i = 0; i<15; i++){
-		aiCards.clear();
-		Cards.clear();
-		
-		deck = new Deck();
-		deck.shuffle();
-		getCards();
+	/**
+	 * 
+	 * @param cards gets card that are important for this turn.
+	 */
+	public Hand(ArrayList<Card> cards) {
+		this.cards = cards;
 		convertToReadable();
-	
-		calc = new Calc(aiCards);
-		String helper = calc.Help();	
+
+		calc = new HandCalculation(aiCards);
+		String helper = calc.Help();
 		toHighlight = calc.toHiglight();
-		
-		
-		System.out.println(aiCards);	
+
+		System.out.println(" -NEW HAND- ");
+		System.out.println(aiCards);
 		System.out.println("Helper - " + helper);
 		System.out.println("");
 		System.out.println("toHighlight - " + toHighlight);
 		System.out.println("");
 		System.out.println("");
 	}
-	}
-	
-	public void convertToReadable(){
-		
-		for (int i = 0; i < Cards.size(); i++) {
-			Card cardTemp = Cards.get(i);
+	/**
+	 * Converts the cards into readable Strings.
+	 */
+	public void convertToReadable() {
+
+		for (int i = 0; i < cards.size(); i++) {
+			Card cardTemp = cards.get(i);
 			char A = cardTemp.getCardSuit().charAt(0);
-			String temp = cardTemp.getCardValue()+","+String.valueOf(A);
+			String temp = cardTemp.getCardValue() + "," + String.valueOf(A);
 			aiCards.add(temp);
 		}
-		
-		
-//		aiCards.add("4,C");
-//		aiCards.add("8,D");
-//		aiCards.add("6,H");
-//		aiCards.add("5,C");
-//		aiCards.add("7,C");
-//		aiCards.add("13,D");
-		
-	}
-	
-	
-	
-	public void getCards() {
 
-		for (int i = 0; i < 2; i++) {
-			Cards.add(deck.getCard());
-
-		}
 	}
-	
-	public ArrayList sendToHighlight(){
+	/**
+	 * @return returns what is suppost to be highlighted.
+	 */
+	public ArrayList<String> sendToHighlight() {
 		return toHighlight;
 	}
-	
-	
-	
-	public static void main(String[] args) {
 
-		hand run = new hand();
-	}
 }
