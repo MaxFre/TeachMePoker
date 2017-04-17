09a58dc65ac8666d7e43e7bc59bc1b63737d9f29
Lykke Lewin <lykkelewin@gmail.com>
2017-04-17 23:27:31 +0200

    Test syso kod

--- src/gui/testGUIPackages/TestTeachMePoker/src/testLykkeMenu/Controller.java
+++ src/gui/testGUIPackages/TestTeachMePoker/src/testLykkeMenu/Controller.java
@@ -7,13 +7,27 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 
 public class Controller {
+	
+	@FXML
 	private Button btnNewGame;
-	private Main main;
+	@FXML
+	private TextField tfNameInput;
+	@FXML
+	private Slider aiSlider;
+	@FXML
+	private Slider potSlider;
+	@FXML 
+	private ToggleButton tbShowTutorial;
+	
+
 	Stage window;
 	Scene sceneNewGame;
 	
@@ -26,9 +40,29 @@ public class Controller {
 	public void btnNewGameClicked() throws IOException{
 		window = new Stage();
 		Parent rootNewGame = FXMLLoader.load(getClass().getResource("NewGameMenu.fxml"));
-		sceneNewGame = new Scene(rootNewGame, 1100, 640);
+		sceneNewGame = new Scene(rootNewGame, 1151, 694);
 		window.setScene(sceneNewGame);
 		window.show();
 		System.out.println("Clicked button");
+	}
+	
+	public void btnLoadGameClicked(){
+		
+	}
+	
+	public void tfNameInputChange(){
+		System.out.println("cool");
+	}
+	
+	public void aiSliderChange(){
+		System.out.println("Slider moved");
+	}
+	
+	public void potSliderChange(){
+		System.out.println("Slider moved");
+	}
+	
+	public void tbChanged(){
+		System.out.println("Toggle button changed");
 	}
 }
