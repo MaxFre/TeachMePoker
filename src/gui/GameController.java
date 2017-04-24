02d7b15de4e2f230edd607beebae39aa7a82d58a
aharirchian <harirchian.amin@gmail.com>
2017-04-24 23:28:02 +0200

    Update och kommenterad GameController ochGameState

--- src/gui/GameController.java
+++ src/gui/GameController.java
@@ -17,282 +17,268 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 public class GameController {
-  
-  private ChangeScene changeScene;
-  
-  @FXML
-  private TextField field;
-  @FXML
-  private Slider slider;
-  @FXML
-  private Label label;
-  @FXML
-  private Label potValueLabel;
 
-  @FXML
-  private Button okBox;
-  @FXML
-  private ImageView arrow;
-
-  @FXML
-  private ImageView cardOne;
-
-  @FXML
-  private Pane pane;
-
-  @FXML
-  private Button next;
-
-  @FXML
-  private Label adviceLabel;
-
-  @FXML
-  private Label helpLabel;
-
-  @FXML
-  private Label userName;
-
-
-  private Image image;
-  private ArrayList<String> cardsIcon = new ArrayList<String>();
-  private Hand hand;
-  private Deck deck;
-  private double bet;
-  private double potValue = 2000; // just for test
-  private static final double INIT_VALUE = 50;
-  ImageView imgView = new ImageView();
-  ImageView imgView2 = new ImageView();
-  private FileHandler pot;
-  
-public void initialize() throws Exception{
-    
-  }
-  
-  public void exitGame() {
-
-    System.exit(0);
-  }
-  
-  public void setChangeScene(ChangeScene sceneChanger) {
-    this.changeScene = sceneChanger;
-  }
-
-
-  public void playerCheck() {
-
-    System.out.println("Player checked");
-  }
-
-
-  public void playerFold() {
-
-    System.out.println("Player folded");
-    label.setText("fold");
-  }
-
-
-  public void PlayerBet() {
-
-    System.out.println("Player bets");
-  }
-
-
-  public void playerRaise() {
-
-    System.out.println("Player raised");
-  }
-
-
-  public void newGame() {
-
-    System.out.println("new game");
-  }
-
-
-  public void saveGame() {
-
-    try {
-      // System.out.println(potValue); // test for potvalue amount
-      pot = new FileHandler(potValue);
-      pot.savePot();
-      System.out.println("pot from GUI:" + pot);
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-  }
-
-
-  public void sliderChange() {
-
-    // Double val = slider.getValue();
-    // field.setText(val.toString());
-    slider.setValue(Math.round(INIT_VALUE));
-    field.setText(new Double(Math.round(INIT_VALUE)).toString());
-    field.textProperty().bindBidirectional(slider.valueProperty(),
-        NumberFormat.getNumberInstance());
-
-  }
-
-
-  // player accepts the odds from slider
-  public void okBox() {
-
-    ArrayList<Card> cards = new ArrayList<Card>();
-    ArrayList<String> highligtedCards = new ArrayList<String>();
-    String[] test;
-
-    highligtedCards.clear();
-    cards.clear();
-    deck = new Deck();
-    deck.shuffle();
-    cards.add(deck.getCard());
-    cards.add(deck.getCard());
-    hand = new Hand(cards);
-
-
-    highligtedCards = hand.sendToHighlight();
-
-    test = new String[highligtedCards.size()];
-
-    for (int i = 0; i < test.length; i++) {
-      String firstTemp = highligtedCards.get(i);
-      String[] splitter = firstTemp.split(",");
-      test[i] = splitter[0] + splitter[1];
-    }
-
-
-    int temp = (cards.get(0).getCardValue());
-    int temp3 = (cards.get(1).getCardValue());
-    String temp2 = String.valueOf(temp);
-    String temp4 = String.valueOf(temp3);
-    char A = cards.get(0).getCardSuit().charAt(0);
-    char B = cards.get(1).getCardSuit().charAt(0);
-    temp2 += String.valueOf(A);
-    temp4 += String.valueOf(B);
-
-    System.out.println(temp2);
-    System.out.println(temp4);
-    for (int i = 0; i < test.length; i++) {
-      if (test[i].equals(temp2)) {
-        temp2 += "O";
-      }
-      if (test[i].equals(temp4)) {
-        temp4 += "O";
-      }
-    }
-    // String cardOne= "resources/images/"+temp2+".png";
-    String cardOne = "resources/images/" + temp2 + ".png";
-    String cardTwo = "resources/images/" + temp4 + ".png";
-
-    System.out.println("temp2 - " + temp2);
-    System.out.println("temp4 - " + temp4);
-
-
-    bet = 0;
-    try {
-      // image = null;
-      // imgView.setImage(null);
-      // imgView2.setImage(null);
-      // System.gc();
-      if (potValue > 0.1) {
-        bet = Math.round(slider.getValue());
-        double NewPotValue = potValue - bet;
-        System.out.println("" + NewPotValue);
-        label.setText("" + bet + " §");
-        potValue = NewPotValue;
-        potValueLabel.setText("" + NewPotValue + " §");
-        slider.setMax(NewPotValue);
-
-        // changePosition(); //extra
-        arrow.setLayoutX(437);
-        arrow.setLayoutY(66);
-
-        // help.setText("ses");
-        String helpText = hand.theHelp();
-        helpLabel.setText(helpText);
-        String adviceText = hand.theAdvice();
-        adviceLabel.setText("Råd: \n" + adviceText);
-
-        System.out.println("temp " + temp2);
-        System.out.println("temp " + temp4);
-
-        // test transfer card
-        // image = new Image(getClass().getResourceAsStream(cardOne),80,116,false,false);
-        image = new Image(Paths.get(cardOne).toUri().toString(), 80, 116, false, false);
-        // Image newimg = image.getScaledInstance(120, 120, 120);
-        imgView = new ImageView(image);
-        pane.getChildren().add(imgView);
-        imgView.setX(0);
-        imgView.setY(0);
-
-        // image = new Image(getClass().getResourceAsStream(cardTwo),80,116, false,false);
-        image = new Image(Paths.get(cardTwo).toUri().toString(), 80, 116, false, false);
-        imgView2 = new ImageView(image);
-        pane.getChildren().add(imgView2);
-        imgView2.setX(70);
-        imgView2.setY(0);
-      } else {
-        potValueLabel.setText("0.0");
-        field.setText("0.0");
-        field.setDisable(true);
-        slider.setDisable(true);
-
-
-      }
-    } catch (Exception e) {
-
-      // okBox.setVisible(false);
-      okBox.setDisable(true); // something wrong ???
-    }
-
-  }
-
-
-  public void deactiveButton() { // extra function (just in case)
-
-    okBox.setVisible(false);
-
-  }
-
-
-  public void soundSetting() {
-
-    System.out.println("Sound Setting");
-  }
-
-
-  public void helpState() {
-
-    System.out.println("Go to Rules section");
-  }
-
-
-  public void mainState() {
-
-    System.out.println("Go to Main section");
-  }
-
-
-  public double setPotValue() {
-
-    System.out.println(potValue);
-    return potValue;
-  }
-
-
-  public void nextRound() {
-
-    image = null;
-    imgView.setImage(null);
-    imgView2.setImage(null);
-    System.gc();
-  }
-
-  public void setUsername(String name) {
-    
-    userName.setText(name);
-    
-  }
+	@FXML
+	private ImageView btCall;
+	@FXML
+	private ImageView btFold;
+	@FXML
+	private ImageView btRaise;
+	@FXML
+	private Slider slider;
+	@FXML
+	private Label lbPlayerAction;
+	@FXML
+	private Label lbPotValue;
+	@FXML
+	private Label lbAllIn;
+	@FXML
+	private Pane powerBarArea;
+	@FXML
+	private ImageView cardOne;
+	@FXML
+	private Pane playerCardsArea;
+	@FXML
+	private Label adviceLabel;
+	@FXML
+	private Label helpLabel;
+	@FXML
+	private Label userName;
+	private ChangeScene changeScene;
+	private int powerBarValue = 0;
+	private Image image;
+	private ArrayList<String> cardsIcon = new ArrayList<String>();
+	private Hand hand;
+	private Deck deck;
+	private int bet;
+	private int potValue = 2000; // just for testing, its coming from controller
+	private ImageView imgCardOne = new ImageView();
+	private ImageView imgCard2 = new ImageView();
+	private ImageView imgPowerBar = new ImageView();
+	private FileHandler pot;
+
+	public void initialize() throws Exception {
+
+	}
+
+	public void exitGame() {
+
+		System.exit(0);
+	}
+
+	public void setChangeScene(ChangeScene sceneChanger) {
+		this.changeScene = sceneChanger;
+	}
+
+	public void playerCheck() {
+		System.out.println("Player checked");
+	}
+
+	public void playerFold() {
+		System.out.println("Player folded");
+		lbPlayerAction.setText("fold");
+	}
+
+	public void PlayerCall() {
+		System.out.println("Player call");
+	}
+
+	public void playerRaise() {
+		System.out.println("Player raised");
+	}
+
+	public void newGame() {
+		System.out.println("new game");
+	}
+
+	public void saveGame() {
+		try {
+			pot = new FileHandler(potValue);
+			pot.savePot();
+			System.out.println("pot from GUI:" + pot);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void sliderChange() {
+		int val = (int) slider.getValue();
+		// ****************** if we like to show the bet value in the text field ******************
+		// field.setText(""+val);
+		// slider.setValue((int)(INIT_VALUE));
+		// field.setText(new Integer((int)(INIT_VALUE)).toString());
+		// field.textProperty().bindBidirectional(slider.valueProperty(),
+		// NumberFormat.getNumberInstance());
+		// ****************************************************************************************
+	}
+
+	// this method uses only for testing Hand and deck as a demo
+	// Okbox method starts when you click the Raise button and shows the player
+	// cards, help, advise and power bar
+	public void okBox() {
+		nextRound(); // method that restart all the images.
+		// get Cards from deck
+		// XXXXXXXXXXXXXXXXXXXXXXX This code is a testing for hand and deck XXXXXXXXXXXXXXXXXXXXXXXXXXX
+		ArrayList<Card> cards = new ArrayList<Card>();
+		ArrayList<String> highligtedCards = new ArrayList<String>();
+		String[] test;
+
+		highligtedCards.clear();
+		cards.clear();
+		deck = new Deck();
+		deck.shuffle();
+		cards.add(deck.getCard());
+		cards.add(deck.getCard());
+		hand = new Hand(cards);
+
+		highligtedCards = hand.sendToHighlight();
+
+		test = new String[highligtedCards.size()];
+
+		for (int i = 0; i < test.length; i++) {
+			String firstTemp = highligtedCards.get(i);
+			String[] splitter = firstTemp.split(",");
+			test[i] = splitter[0] + splitter[1];
+		}
+
+		int temp = (cards.get(0).getCardValue());
+		int temp3 = (cards.get(1).getCardValue());
+		String temp2 = String.valueOf(temp);
+		String temp4 = String.valueOf(temp3);
+		char A = cards.get(0).getCardSuit().charAt(0);
+		char B = cards.get(1).getCardSuit().charAt(0);
+		temp2 += String.valueOf(A);
+		temp4 += String.valueOf(B);
+
+		System.out.println(temp2);
+		System.out.println(temp4);
+		for (int i = 0; i < test.length; i++) {
+			if (test[i].equals(temp2)) {
+				temp2 += "O";
+			}
+			if (test[i].equals(temp4)) {
+				temp4 += "O";
+			}
+		}
+		String cardOne = "resources/images/" + temp2 + ".png";
+		String cardTwo = "resources/images/" + temp4 + ".png";
+		String powerBarWeakHand = "resources/images/weakHand.png";
+		String powerBarMediumWeakHand = "resources/images/mediumWeakHand.png";
+		String powerBarMediumStrongHand = "resources/images/mediumStrongHand.png";
+		String powerBarStrongHand = "resources/images/StrongHand.png";
+
+		System.out.println("temp2 - " + temp2);
+		System.out.println("temp4 - " + temp4);
+		// XXXXXXXXXXXXXXXXXXXXXXXXXXX END XXXXXXXXXXXXXXXXXXXXX
+
+		bet = 0;
+		try {
+
+			bet = (int) (slider.getValue());
+			if (potValue >= bet) {
+				lbPlayerAction.setText("" + bet + " §");
+				int NewPotValue = potValue - bet;
+				System.out.println("" + NewPotValue);
+
+				potValue = NewPotValue;
+				lbPotValue.setText("" + NewPotValue + " §");
+				slider.setMax(NewPotValue);
+				if (bet == potValue) {
+					lbPlayerAction.setText("ALL IN");
+					lbPotValue.setText("" + 0 + " §");
+					slider.setMax(NewPotValue);
+					showAllIn();
+				}
+				String helpText = hand.theHelp();
+				helpLabel.setText(helpText);
+				String adviceText = hand.theAdvice();
+				adviceLabel.setText("Råd: \n" + adviceText);
+
+				powerBarValue = hand.toPowerBar();
+				if (powerBarValue == 1) {
+					image = new Image(Paths.get(powerBarWeakHand).toUri().toString(), 120, 166, false, false);
+					imgPowerBar = new ImageView(image);
+					powerBarArea.getChildren().add(imgPowerBar);
+					imgPowerBar.setX(30);
+					imgPowerBar.setY(15);
+				}
+				if (powerBarValue == 2) {
+					image = new Image(Paths.get(powerBarMediumWeakHand).toUri().toString(), 120, 166, false, false);
+					imgPowerBar = new ImageView(image);
+					powerBarArea.getChildren().add(imgPowerBar);
+					imgPowerBar.setX(30);
+					imgPowerBar.setY(15);
+				}
+				if (powerBarValue == 3) {
+					image = new Image(Paths.get(powerBarMediumStrongHand).toUri().toString(), 120, 166, false, false);
+					imgPowerBar = new ImageView(image);
+					powerBarArea.getChildren().add(imgPowerBar);
+					imgPowerBar.setX(30);
+					imgPowerBar.setY(15);
+				}
+				if (powerBarValue == 4) {
+					image = new Image(Paths.get(powerBarStrongHand).toUri().toString(), 120, 166, false, false);
+					imgPowerBar = new ImageView(image);
+					powerBarArea.getChildren().add(imgPowerBar);
+					imgPowerBar.setX(30);
+					imgPowerBar.setY(15);
+
+				}
+
+				image = new Image(Paths.get(cardOne).toUri().toString(), 120, 166, false, false);
+				imgCardOne = new ImageView(image);
+				playerCardsArea.getChildren().add(imgCardOne);
+				imgCardOne.setX(0);
+				imgCardOne.setY(0);
+				image = new Image(Paths.get(cardTwo).toUri().toString(), 120, 166, false, false);
+				imgCard2 = new ImageView(image);
+				playerCardsArea.getChildren().add(imgCard2);
+				imgCard2.setX(105);
+				imgCard2.setY(0);
+			} else {
+				lbPotValue.setText("0.0");
+				showAllIn();
+				slider.setDisable(true);
+			}
+		} catch (Exception e) {
+		}
+	}
+
+	public void soundSetting() {
+		System.out.println("Sound Setting");
+	}
+
+	public void rulesState() {
+		System.out.println("Go to Rules section");
+	}
+
+	public void mainState() {
+		System.out.println("Go to Main section");
+	}
+
+	public double setPotValue() {
+		System.out.println(potValue);
+		return potValue;
+	}
+
+	// restart all the images
+	public void nextRound() {
+
+		image = null;
+		imgCardOne.setImage(null);
+		imgCard2.setImage(null);
+		imgPowerBar.setImage(null);
+		System.gc();
+	}
+
+	public void setUsername(String name) {
+		userName.setText(name);
+
+	}
+
+	public void showAllIn() {
+		lbAllIn.setVisible(true);
+	}
 
 }
