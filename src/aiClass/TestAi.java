945cdda3675e12caae1bcb0b787476fb9780a71c
MaxFre <mafr@live.se>
2017-05-04 11:08:45 +0200

    till sprint 3

--- src/aiClass/TestAi.java
+++ src/aiClass/TestAi.java
@@ -1,9 +1,22 @@
 package aiClass;
 
 
-import deck.Card;
-import deck.Deck;
+import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+import deck.Card;
+import deck.CardValue;
+import deck.Deck;
+import deck.Suit;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 /**
  * Class that is for testing of the AI.
  * 
@@ -11,7 +24,7 @@ import deck.Deck;
  * @version 1.5
  */
 public class TestAi {
-	Deck deck;
+  Deck deck;
   Ai ai;
   private Card card1;
   private Card card2;
@@ -19,31 +32,69 @@ public class TestAi {
   private Card cardTurn;
   private Card cardRiver;
 
+  private ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+  private ArrayList<Card> decken = new ArrayList<Card>();
+  
   public TestAi() {
+	  decken = new ArrayList<Card>();
 
-    deck = new Deck();
-    deck.shuffle();
+//	 1=2h   14 = 2s   27 = 2c   40=2d 
+//	  2H, 2S , 2D,4C, 3D
+	  deck = new Deck();
+//    deck.shuffle();
 
-    card1 = deck.getCard();
-    deck.shuffle();
-    card2 = deck.getCard();
-    cardTurn = deck.getCard();
-    cardRiver = deck.getCard();
+		for (int i = 0; i < 1; i++) {
+			card1 = deck.getCard();
+		}
+		deck = new Deck();
+		for (int i = 0; i < 2; i++) {
+			card2 = deck.getCard();
+		}
+    //FLOPP
+		deck = new Deck();
+		for (int i = 0; i < 3; i++) {
+			flop[0] = deck.getCard();
+		}
+		
+		deck = new Deck();
+		for (int i = 0; i < 4; i++) {
+			flop[1] = deck.getCard();
+		}
+		deck = new Deck();
+		for (int i = 0; i < 5; i++) {
+			flop[2] = deck.getCard();
+		}
+//   //TURN
+//		deck = new Deck();
+//		for (int i = 0; i < 16; i++) {
+//			cardTurn = deck.getCard();
+//		}
+//	//RIVER
+//		deck = new Deck();
+//		for (int i = 0; i < 16; i++) {
+//			cardRiver = deck.getCard();
+//		}
+			
+	
+//    card1 = deck.getCard();
+//    card2 = deck.getCard();
+//    cardTurn = deck.getCard();
+//    cardRiver = deck.getCard();
 
-    for (int i = 0; i < 3; i++) { // 3 kort för flopp.
-      deck.shuffle();
-      flop[i] = deck.getCard();
-    }
+//    for (int i = 0; i < 3; i++) { // 3 kort för flopp.
+////      deck.shuffle();
+//      flop[i] = deck.getCard();
+//    }
 
     ai = new Ai(808, "TestAiName");
 
     ai.setStartingHand(card1, card2);
-    ai.makeDecision(150);
-    System.out.println(ai.getDecision());
-
-//    System.out.println("\n\n-Test FLOP-");
-//    ai.makeDecision(32, flop);
+//    ai.makeDecision(150);
 //    System.out.println(ai.getDecision());
+
+    System.out.println("\n\n-Test FLOP-");
+    ai.makeDecision(32, flop);
+    System.out.println(ai.getDecision());
 //
 //    System.out.println("\n\n-Test TURN-");
 //    ai.makeDecision(32, cardTurn);
--- src/hand/TestHand.java
+++ src/hand/TestHand.java
@@ -14,26 +14,64 @@ import deck.Deck;
 public class TestHand {
   Deck deck;
   Hand hand;
+  Card card1;
+  Card card2;
+  Card card3;
+  Card card4;
+  Card card5;
+  
   private ArrayList<Card> cards = new ArrayList<Card>();
   private int[] turn = {2, 5, 6, 7};
 
   public TestHand() {
 
+//		 1=2h   14 = 2s   27 = 2c   40=2d 
+//	  2H, 2S, 2C,4H,4S
+	  deck = new Deck();
+//    deck.shuffle();
 
+		for (int i = 0; i < 1; i++) {
+			card1 = deck.getCard();
+		}
+		deck = new Deck();
+		for (int i = 0; i < 14; i++) {
+			card2 = deck.getCard();
+		}
+    //FLOPP
+		deck = new Deck();
+		for (int i = 0; i < 1; i++) {
+			card3 = deck.getCard();
+		}
+		
+		deck = new Deck();
+		for (int i = 0; i < 1; i++) {
+			card4 = deck.getCard();
+		}
+		deck = new Deck();
+		for (int i = 0; i < 15; i++) {
+			card5 = deck.getCard();
+		}
 	  
-    for (int testAlot = 0; testAlot < 15; testAlot++) {
-      cards.clear();
-      deck = new Deck();
-      deck.shuffle();
+		cards.add(card1);
+		cards.add(card2);
+		cards.add(card3);
+		cards.add(card4);
+		cards.add(card5);
 
-      Random rand = new Random();
-      int RandomSize = rand.nextInt(4);
-
-      int thisTurn = turn[3];
-
-      for (int i = 0; i < thisTurn; i++) {
-        cards.add(deck.getCard());
-      }
+		
+//    for (int testAlot = 0; testAlot < 15; testAlot++) {
+//      cards.clear();
+//      deck = new Deck();
+//      deck.shuffle();
+//
+//      Random rand = new Random();
+//      int RandomSize = rand.nextInt(4);
+//
+//      int thisTurn = turn[3];
+//
+//      for (int i = 0; i < thisTurn; i++) {
+//        cards.add(deck.getCard());
+//      }
 
       hand = new Hand(cards);
       
@@ -41,7 +79,7 @@ public class TestHand {
     }
 		
 
-  }
+//  }
 
   public static void main(String[] args) {
     TestHand run = new TestHand();
