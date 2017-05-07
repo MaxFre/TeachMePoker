71e8725233a5e630f2d253a3ce0b7efd2fa16358
MaxFre <mafr@live.se>
2017-05-07 16:59:16 +0200

    handStrenght inlagd

--- src/hand/TestHand.java
+++ src/hand/TestHand.java
@@ -26,52 +26,52 @@ public class TestHand {
   public TestHand() {
 
 //		 1=2h   14 = 2s   27 = 2c   40=2d 
-//	  2H, 2S, 2C,4H,4S
+
 	  deck = new Deck();
-//    deck.shuffle();
+    deck.shuffle();
 
-		for (int i = 0; i < 1; i++) {
-			card1 = deck.getCard();
-		}
-		deck = new Deck();
-		for (int i = 0; i < 14; i++) {
-			card2 = deck.getCard();
-		}
-    //FLOPP
-		deck = new Deck();
-		for (int i = 0; i < 1; i++) {
-			card3 = deck.getCard();
-		}
-		
-		deck = new Deck();
-		for (int i = 0; i < 1; i++) {
-			card4 = deck.getCard();
-		}
-		deck = new Deck();
-		for (int i = 0; i < 15; i++) {
-			card5 = deck.getCard();
-		}
-	  
-		cards.add(card1);
-		cards.add(card2);
-		cards.add(card3);
-		cards.add(card4);
-		cards.add(card5);
+//		for (int i = 0; i < 1; i++) {
+//			card1 = deck.getCard();
+//		}
+//		deck = new Deck();
+//		for (int i = 0; i < 2; i++) {
+//			card2 = deck.getCard();
+//		}
+//    //FLOPP
+//		deck = new Deck();
+//		for (int i = 0; i < 3; i++) {
+//			card3 = deck.getCard();
+//		}
+//		
+//		deck = new Deck();
+//		for (int i = 0; i < 4; i++) {
+//			card4 = deck.getCard();
+//		}
+//		deck = new Deck();
+//		for (int i = 0; i < 5; i++) {
+//			card5 = deck.getCard();
+//		}
+//	  
+//		cards.add(card1);
+//		cards.add(card2);
+//		cards.add(card3);
+//		cards.add(card4);
+//		cards.add(card5);
 
 		
-//    for (int testAlot = 0; testAlot < 15; testAlot++) {
-//      cards.clear();
-//      deck = new Deck();
-//      deck.shuffle();
-//
-//      Random rand = new Random();
-//      int RandomSize = rand.nextInt(4);
-//
-//      int thisTurn = turn[3];
-//
-//      for (int i = 0; i < thisTurn; i++) {
-//        cards.add(deck.getCard());
-//      }
+    for (int testAlot = 0; testAlot < 15; testAlot++) {
+      cards.clear();
+      deck = new Deck();
+      deck.shuffle();
+
+      Random rand = new Random();
+      int RandomSize = rand.nextInt(4);
+
+      int thisTurn = turn[RandomSize];
+
+      for (int i = 0; i < thisTurn; i++) {
+        cards.add(deck.getCard());
+      }
 
       hand = new Hand(cards);
       
@@ -79,7 +79,7 @@ public class TestHand {
     }
 		
 
-//  }
+  }
 
   public static void main(String[] args) {
     TestHand run = new TestHand();
